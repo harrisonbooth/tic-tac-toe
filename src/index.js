f9a28912edeb3caf40a8a15a31de1791310562ad
Harrison Booth <harrisonbooth@ymail.com>
2017-07-12 10:41:13 +0100

    add index.html with ReactDOM render for TicTacToeContainer, which doesn't exist yet

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import TicTacToeContainer from "./containers/TicTacToeContainer.jsx"
+
+const mountContainer = () => {
+  ReactDOM.render(
+    <TicTacToeContainer />,
+    document.getElementById("tic-tac-toe")
+  )
+}
