160ee3725bc87fd678ace4381bac403b84dff673
Harrison Booth <harrisonbooth@ymail.com>
2017-07-12 13:57:03 +0100

    change display so that it has rows of 3 tiles

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-
+import React from "react"
 import { Tile } from "./Tile.jsx"
 
 export const Board = (props) => {
-  const tiles = props.board.map((tile, index) => {
-    return <Tile onClick={props.onTileClick} symbol={tile} index={index} key={index} />
+  let tilesInRow = []
+  const rows = props.board.map((tile, index) => {
+    tilesInRow.push(
+      <Tile index={index} symbol={tile} 
+      key={index} onClick={props.onTileClick} />
+    )
+    
+    if((index+1)%3 !== 0) return null
+
+    const subArray = tilesInRow
+    tilesInRow = []
+
+    return (
+      <div key={`row-${index/3}`} className="tile-row">
+        {subArray}
+      </div>
+    )
   })
 
   return (
     <div id="game-board">
-      {tiles}
+      {rows}
     </div>
   )
 }
